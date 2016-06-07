3858e1e64a011ff3c456dd143fd45f42c1ea42e5
Jason Jordan <jtjordan13@gmail.com>
2016-06-07 16:23:53 -0400

    Threw everything into methods

--- script_2.rb
+++ script_2.rb
@@ -1,39 +1,37 @@
 require "./db/setup"
 require "./lib/all"
 
-# Prompts the user for their name
+def get_user
+  puts "What is your first name?"
+  first_name = gets.chomp
+  puts "What is your last name?"
+  last_name = gets.chomp
 
-puts "What is your first name?"
-first_name = gets.chomp
-puts "What is your last name?"
-last_name = gets.chomp
+  User.where(first_name: first_name, last_name: last_name).first_or_create!
+end
 
-user = User.where(first_name: first_name, last_name: last_name).first_or_create!
-all_items = Item.all
-loop do
-  # Displays a list of available items
-
+def display_item_list item_list
   puts
   puts "Avaliable Items:"
-  all_items.each do |item|
+  item_list.each do |item|
     puts "#{item.id}) #{item.description}"
   end
+end
 
-  # Asks the user to choose an item
-
+def get_item item_list
   puts "Please choose an item number to purchase."
   print " >"
   item_number = gets.chomp
-  item = all_items.find_by(id: item_number)
+  item = item_list.find_by(id: item_number)
+end
 
-  # Asks the user for a quantity
-
+def get_quantity item
   puts "How many #{item.description}s would you like to purchase?"
   print " >"
   quantity = gets.chomp.to_i
+end
 
-  # Creates a new order for that user / item / quantity
-
+def create_and_display_invoice user, item, quantity
   user_purchase = Purchase.create(user_id: user.id, item_id: item.id, quantity: quantity)
   puts
   puts "---Aardwolf Inc.---".center(40)
@@ -42,11 +40,41 @@ loop do
   puts "#{item.description} x#{user_purchase.quantity}".center(40)
   puts "at $#{user_purchase.item.price.to_f.round(2)} each".center(40)
   puts "total: $#{((user_purchase.item.price.to_f)*(user_purchase.quantity)).round(2)}".center(40)
-  puts "Purchase made at #{user_purchase.created_at}".center(40)
+  puts "Purchase made at".center(40)
+  puts "#{user_purchase.created_at}:".center(40)
   puts "-------------------".center(40)
+end
+
+def additional_purchase?
   puts
   puts "Would you like to make another purchase (y/n)?"
   input = gets.chomp
+end
+
+
+# Prompts the user for their name
+
+user = get_user
+item_list = Item.all
+loop do
+  # Displays a list of available items
+
+  display_item_list item_list
+
+  # Asks the user to choose an item
+
+  item = get_item item_list
+
+  # Asks the user for a quantity
+
+  quantity = get_quantity item
+
+  # Creates a new order for that user / item / quantity
+
+  create_and_display_invoice user, item, quantity
+
+  input = additional_purchase?
+
   unless input == "y"
     break
   end
